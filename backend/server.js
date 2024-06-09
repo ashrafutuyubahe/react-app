cdf99f77c8a7d1b8ee478f07dd52c5bc5ee95615
Ashrafu <ashraftuyubahe001@gmail.com>
2024-06-09 10:37:52 +0200

    working on broaddcsting message between rooms

--- backend/server.js
+++ backend/server.js
@@ -16,18 +16,20 @@ const io = socketIo(server, {
 app.use(cors());
 
 io.on("connect", (socket) => {
-  console.log(`you are now connected with id ${socket.id}`);
+  console.log(`You are now connected with id ${socket.id}`);
 
-  socket.on("messagesent", (message) => {
-    console.log(message);
+  socket.on("messagesent", (message, roomName) => {
+    console.log(message,roomName);
 
-    socket.broadcast.emit("receivedMessage", message);
+    // Broadcast the message to all clients in the specified room
+    io.to(roomName).emit("receivedMessage", message, roomName);
+  });
+
+  socket.on("joinRoom", (roomName) => {
+    // Join the specified room
+    socket.join(roomName);
   });
 });
-
-
-
-
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,26 +6,40 @@ const socket = io.connect("http://localhost:3000");
 function App() {
   const [message, setMessage] = useState("");
   const [receivedMessage, setReceivedMessage] = useState("");
+  const [roomName, setRoomName] = useState("");
 
   useEffect(() => {
     socket.on("connect", () => {
       console.log(`New user with id of ${socket.id} connected`);
+      // Join the room when the client connects
+      socket.emit("joinRoom", roomName);
     });
 
-    socket.on("receivedMessage", (receivedMessage) => {
-      setReceivedMessage(receivedMessage);
+    socket.on("receivedMessage", (receivedMessage, roomReceived) => {
+    
+        setReceivedMessage(receivedMessage);
+      
+   
     });
-  }, []);
+  }, [roomName]);
 
   function handleSendMessage(event) {
     event.preventDefault();
-    socket.emit("messagesent", message);
+    socket.emit("messagesent", message, roomName);
     setMessage("");
   }
 
   return (
     <div className="App">
       <h3>Hello, this is a chat application</h3>
+      <input
+        type="text"
+        placeholder="Enter the name of the room you would like to join"
+        onChange={(e) => {
+          setRoomName(e.target.value);
+          console.log(roomName);
+        }}
+      />
       <form onSubmit={handleSendMessage}>
         <input
           type="text"
@@ -35,9 +49,10 @@ function App() {
         <button type="submit">Send message</button>
       </form>
 
-    
       {receivedMessage && (
-        <h3 style={{ display: "block" }}>Received message: {receivedMessage}</h3>
+        <h3 style={{ display: "block" }}>
+          Received message: {receivedMessage}
+        </h3>
       )}
     </div>
   );
